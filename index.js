0256efcd0978fe560a7c81fcee818328a4cf0a98
Ben Clark <benclarkphoto@mac.com>
2018-06-22 23:58:24 -0700

    https://github.com/BenShigeruClark/FirebaseAssignment_TrainScheduler

--- index.js
+++ index.js
@@ -12,10 +12,15 @@
 
   firebase.initializeApp(config);
 
+    // variable to reference database.
   var database = firebase.database();
 
+    //Button Click function 
    $("#submitButton").on("click", function() {
+     
 
+
+      // Values from the text boxes
             var trainName = $('#trainNameInput').val().trim();
             var destination = $('#destinationInput').val().trim();
             var firstTrainTime = $('#firstTrainTimeInput').val().trim();
@@ -23,39 +28,50 @@
             var nextArrival = moment($("#firstTrainTimeInput").val().trim()).format("hh:mm:ss");
             var minutesAway = $('minutesAway').val().trim();
 
-            console.log(trainName);
-            console.log(destination);
-            console.log(firstTrainTime);
-            console.log(frequency);
-            console.log(nextArrival);
-            console.log(minutesAway);
+            // console.log(trainName);
+            // console.log(destination);
+            // console.log(firstTrainTime);
+            // console.log(frequency);
+            // console.log(nextArrival);
+            // console.log(minutesAway);
 
   // creating a local object that will be pushed to firebase
-            var nextTrain = {
+           database.ref().push({
               trainName: trainName,
               destination: destination,
               firstTrainTime: firstTrainTime,
               frequency: frequency,
               nextArrival: nextArrival,
               minutesAway: minutesAway, 
-            }
+            });
         
             database.push(nextTrain);
+        
 
+      //Clear textboxes
             $("#trainNameInput").val("");
             $("#destinationInput").val("");
             $("#firstTrainTimeInput").val("");
             $("#frequencyInput").val("");
            
+            // prevent page from refreshing
             return false;
         
           });
 
 
 
-          database.on("child_added", function(childSnapshot, prevChildKey) {
+          database.on("child_added", function(childSnapshot) {
               
               console.log(childSnapshot.val());
+              console.log(childSnapshot.val().trainName);
+              console.log(childSnapshot.val().destination);
+              console.log(childSnapshot.val().firstTrainTime);
+              console.log(childSnapshot.val().frequency);
+              console.log(childSnapshot.val().nextArrival);
+              console.log(childSnapshot.val().minutesAway);
+
+
 
               var firebaseTrainName = childSnapshot.val().trainName;
               console.log(firebaseTrainName);
