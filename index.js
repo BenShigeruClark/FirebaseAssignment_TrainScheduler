b6beec97135396e0ffa97a4c88e4637a5622c610
Ben Clark <benclarkphoto@mac.com>
2018-06-22 22:22:45 -0700

    https://github.com/BenShigeruClark/FirebaseAssignment_TrainScheduler

--- index.js
+++ index.js
@@ -7,27 +7,117 @@
     storageBucket: "",
     messagingSenderId: "49992363112"
   };
+
+
+
   firebase.initializeApp(config);
 
+  var database = firebase.database();
+
+   $("#submitButton").on("click", function() {
+
+            var trainName = $('#trainNameInput').val().trim();
+            var destination = $('#destinationInput').val().trim();
+            var firstTrainTime = $('#firstTrainTimeInput').val().trim();
+            var frequency = $('#frequencyInput').val().trim();
+            var nextArrival = moment($("#firstTrainTimeInput").val().trim()).format("hh:mm:ss");
+            var minutesAway = $('minutesAway').val().trim();
+
+            console.log(trainName);
+            console.log(destination);
+            console.log(firstTrainTime);
+            console.log(frequency);
+            console.log(nextArrival);
+            console.log(minutesAway);
+
+  // creating a local object that will be pushed to firebase
+            var nextTrain = {
+              trainName: trainName,
+              destination: destination,
+              firstTrainTime: firstTrainTime,
+              frequency: frequency,
+              nextArrival: nextArrival,
+              minutesAway: minutesAway, 
+            }
+        
+            database.push(nextTrain);
+
+            $("#trainNameInput").val("");
+            $("#destinationInput").val("");
+            $("#firstTrainTimeInput").val("");
+            $("#frequencyInput").val("");
+           
+            return false;
+        
+          });
+
+
+
+          database.on("child_added", function(childSnapshot, prevChildKey) {
+              
+              console.log(childSnapshot.val());
+
+              var firebaseTrainName = childSnapshot.val().trainName;
+              console.log(firebaseTrainName);
+          })
+
+
+
+
+          $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + firstTrainTime + 
+          "</td><td>" + frequency + "mins" + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+
+
+
+
+
+
+
 // create variable to reference Firebase
-var database = firebase().database;
+// var database = firebase().database;
 
 // variables referencing data column's
- var trainName = "";
- var destination = "";
- var frequency = "";
- var nextArrival = "";
- var minutesAway = "";                          
+//  var trainName = "";
+//  var destination = "";
+//  var firstTrainTime = "";
+//  var frequency = "";
+//  var nextArrival = "";
+//  var minutesAway = "";                          
 
 
 // Get Submit button to work when clicked 
- $(".btn btn-primary btn-lg").on("click", function(event) {
+//  $(".btn btn-primary btn-lg").on("click", function(event) {
+//    event.preventDefault();
+   //when button is clicked data should be sent to firebase and 
+  //  logged into the page.
+  // Get inputs
+//   trainName = $('#trainNameInput').val().trim();
+//   destination = $('#destinationInput').val().trim();
+//   firstTrainTime = $('#firstTrainTimeInput').val().trim();
+//   frequency = $('#frequencyInput').val().trim();
+ 
+//   database.ref().set({
+//     trainName: trainName,
+//     destination: destination,
+//     firstTrainTime: firstTrainTime,
+//     frequency: frequency
+//     })
+// })
+  
+//   database.ref().on('value', function(snapshot) {
+//     console.log(snapshot);
 
- })
+//     console.log(snapshot.val().trainName);
+//     console.log(snapshot.val().destination);
+//     console.log(snapshot.val().firstTrainName);
+//     console.log(snapshot.val().frequency);
+//   });
+
+  // Get data to add onto each additional data entry
+// make sure that the input data is logged and saved in schedule
+
 
 
   /* Get input fields to send accepted data to table for Current 
   Train Schedule when Submit button is pressed */
 
-// Get data to add onto each additional data entry
-// make sure that the input data is logged and saved in schedule
