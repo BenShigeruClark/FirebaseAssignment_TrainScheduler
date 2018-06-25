4b491b84ad764a8f16510bd5f289620d9fb30623
Ben Clark <benclarkphoto@mac.com>
2018-06-24 23:25:37 -0700

    https://github.com/BenShigeruClark/FirebaseAssignment_TrainScheduler

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
       // Values from the text boxes
             var trainName = $("#trainNameInput").val().trim();
             var destination = $("#destinationInput").val().trim();
-            var firstTrainTime = moment($("#firstTrainTimeInput").val().trim(), "MM/DD/YYYY").format("X");
+            var firstTrainTime = moment($("#firstTrainTimeInput").val().trim(), "hh:mm").format("LT");
             var frequency = $("#frequencyInput").val().trim();
             // var nextArrival = moment($("#firstTrainTimeInput").val().trim()).format("hh:mm:ss");
             // var minutesAway = $('minutesAway').val().trim();
@@ -68,105 +68,46 @@ $(document).ready(function() {
 
           database.ref().on("child_added", function(childSnapshot) {
             console.log(childSnapshot.val());
-          })
+
+            //store everything in variable
+            var trainName = childSnapshot.val().trainName;
+            var destination = childSnapshot.val().destination;
+            var firstTrainTime = childSnapshot.val().firstTrainTime;
+            var frequency = childSnapshot.val().frequency;
+
+            //Train info
+            console.log(trainName);
+            console.log(destination);
+            console.log(firstTrainTime);
+            console.log(frequency);
+
+            //getting data for next arrival and minutes away
+            //Calculate the Next Arrival
+            var nextArrival = moment().endOf("HH:MM").fromNow(firstTrainTime);
+            console.log(nextArrival);
+
+            //Calculate the Minutes Away
+            var minutesAway = moment().startOf("HH:MM").fromNow(frequency);
+            console.log(minutesAway); 
+
+            //Create new rows of data in html
+            var newData = $("<tr>").prepend(
+              $("<td>").text(trainName),
+              $("<td>").text(destination),
+              $("<td>").text(frequency),
+              $("<td>").text(nextArrival),
+              $("<td>").text(minutesAway)
+            );
+              $("#trainTable").prepend(newData);
+            //Append the new row to the data table
+        
+          });
 
         });
-           //log to console
-          //  console.log(newTrain.trainName);
-          //  console.log(newTrain.destination);
-          //  console.log(newTrain.firstTrainTime);
-          //  console.log(newTrain.frequency);
+         
 
            
          
         
 
-      // //Clear textboxes
-      //       $("#trainNameInput").val("");
-      //       $("#destinationInput").val("");
-      //       $("#firstTrainTimeInput").val("");
-      //       $("#frequencyInput").val("");
-           
-      //       // prevent page from refreshing
-      //       return false;
-        
-      //     });
-
-
-
-      //     database.on("child_added", function(childSnapshot) {
-              
-      //         console.log(childSnapshot.val());
-      //         console.log(childSnapshot.val().trainName);
-      //         console.log(childSnapshot.val().destination);
-      //         console.log(childSnapshot.val().firstTrainTime);
-      //         console.log(childSnapshot.val().frequency);
-      //         console.log(childSnapshot.val().nextArrival);
-      //         console.log(childSnapshot.val().minutesAway);
-
-
-
-      //         var firebaseTrainName = childSnapshot.val().trainName;
-      //         console.log(firebaseTrainName);
-      //     })
-
-
-
-
-      //     $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + firstTrainTime + 
-      //     "</td><td>" + frequency + "mins" + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
-
-
-
-
-
-
-
-// create variable to reference Firebase
-// var database = firebase().database;
-
-// variables referencing data column's
-//  var trainName = "";
-//  var destination = "";
-//  var firstTrainTime = "";
-//  var frequency = "";
-//  var nextArrival = "";
-//  var minutesAway = "";                          
-
-
-// Get Submit button to work when clicked 
-//  $(".btn btn-primary btn-lg").on("click", function(event) {
-//    event.preventDefault();
-   //when button is clicked data should be sent to firebase and 
-  //  logged into the page.
-  // Get inputs
-//   trainName = $('#trainNameInput').val().trim();
-//   destination = $('#destinationInput').val().trim();
-//   firstTrainTime = $('#firstTrainTimeInput').val().trim();
-//   frequency = $('#frequencyInput').val().trim();
- 
-//   database.ref().set({
-//     trainName: trainName,
-//     destination: destination,
-//     firstTrainTime: firstTrainTime,
-//     frequency: frequency
-//     })
-// })
-  
-//   database.ref().on('value', function(snapshot) {
-//     console.log(snapshot);
-
-//     console.log(snapshot.val().trainName);
-//     console.log(snapshot.val().destination);
-//     console.log(snapshot.val().firstTrainName);
-//     console.log(snapshot.val().frequency);
-//   });
-
-  // Get data to add onto each additional data entry
-// make sure that the input data is logged and saved in schedule
-
-
-
-  /* Get input fields to send accepted data to table for Current 
-  Train Schedule when Submit button is pressed */
 
